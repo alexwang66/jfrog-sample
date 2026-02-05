64ecb3bae3d07473e07ec655847f6fd02e6f9026
Qing Wang <wq237wq@gmail.com>
2026-02-05 09:28:07 +0100

    add mal model

--- huggingface-sample/download_model.py
+++ huggingface-sample/download_model.py
@@ -7,7 +7,7 @@ if not RT_token:
     raise ValueError("Please set the SOLENG_IO_TOKEN environment variable.")
 
 # Model name
-model_name = "huggingbob/test"  # Replace with the model you want to download
+model_name = "glockr1/ballr7"  # Replace with the model you want to download
 
 # Download the model snapshot
 snapshot_download(
